c59c8269b02fc56123b39aef093a360d40ff8b66
15017470 <15017470@myrp.edu.sg>
2017-04-25 10:42:09 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017470/demolistview/Food.java
@@ -0,0 +1,20 @@
+package com.example.a15017470.demolistview;
+
+/**
+ * Created by 15017470 on 25/4/2017.
+ */
+
+public class Food {
+    private String name;
+    private boolean star;
+    public Food(String name, boolean star) {
+        this.name = name;
+        this.star = star;
+    }
+    public String getName() {
+        return name;
+    }
+    public boolean isStar() {
+        return star;
+    }
+}
